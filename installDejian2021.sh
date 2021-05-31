9555c487b15b5c5280011b579693773fb6f59cb1
Asfffffd <84439113+Asfffffd@users.noreply.github.com>
2021-05-31 11:49:35 +0200

    Update installDejian2021.sh

--- installDejian2021.sh
+++ installDejian2021.sh
@@ -6,10 +6,8 @@ git clone https://gitlab.dune-project.org/core/dune-istl.git
 git clone https://gitlab.dune-project.org/extensions/dune-foamgrid.git
 git clone https://gitlab.dune-project.org/extensions/dune-alugrid.git
 git clone https://git.iws.uni-stuttgart.de/dumux-repositories/dumux.git
-git clone https://github.com/twophaseDFM/Dejian_DFM2PTH.git
+git clone https://github.com/twophaseDFM/DFM2PTH.git
 ./dune-common/bin/dunecontrol --opts=dumux/cmake.opts all
-cd DFM2PTH
-cmake build-cmake
 
 
 
